62570a1afed7393b480f58d7d4cb369e3990a239
itadmin <ccasbre27@hotmail.com>
2015-12-25 16:31:36 -0600

    init

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		// número entero
+		int edad = 9;
+		
+		// número decimal
+		double altura = 175.5;
+		
+		// cadena de caracteres
+		String cadenaCaracteres = "Soy una cadena de caracteres";
+		
+		// boolean
+		boolean administrador = true;
+		
+		System.out.println("Variable entera: " + edad);
+		System.out.println("Variable decimal: " + altura);
+		System.out.println("Variable cadena de caracteres: " + cadenaCaracteres);
+		System.out.println("Variable boolean: " + administrador);
+
+	}
+
+}
